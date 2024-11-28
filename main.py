210c95ed776e149aacec19673e515675f2e61cc5
xynlophyl <nlin2001lfc@gmail.com>
2024-11-28 01:38:00 +0000

    streamline main, add funcdtionality toteam assignment

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import numpy as np
 from pitchlocalization import KeypointDetector, PitchFrame, ViewTransformer
 from team_assigner import TeamAssigner
 from tracker import Tracker, PlayerBallAssigner
-from utils import read_video, save_video, calculate_centroid, measure_distance, get_center_of_bbox
-import random
+from utils import read_video, save_video
 
 def main():
     
@@ -47,62 +46,17 @@ def main():
     TRACKING: team assignment 
     """
     print("assigning team")
-
+    team_assigner = TeamAssigner()
 
     # make sure the team color proportions from k mean makes sense
-    team_assigner = TeamAssigner()
-    for _ in range(10):
-        random_frame_index = random.randrange(len(vod_frames))
-        team_color_counter = [0, 0]
-        team_assigner = TeamAssigner()
-        team_assigner.assign_team_color(vod_frames[random_frame_index], tracks['players'][random_frame_index])
-        for player_id, track in  tracks['players'][random_frame_index].items():
-            if track["cls_name"] == "player":
-                team = team_assigner.get_player_team(vod_frames[random_frame_index],   
-                                                    track,
-                                                    player_id)
-                team_color_counter[team-1] += 1
-
-        total_players = sum(team_color_counter)
-        smaller_team = min(team_color_counter)
-        if smaller_team > total_players * 0.4: # if the smaller team size is at least 40% of the total, the proportions should make sense
-            break
-        else:
-            print("k-mean too biased. re-running")
+    tracks = team_assigner.assign_team_by_sampling(vod_frames, tracks)
 
     # team assignment for goalkeeper 
-    for frame_num, player_track in enumerate(tracks['players']):
-        for player_id, track in player_track.items():
-            if track["cls_name"] == "player":
-                team = team_assigner.get_player_team(vod_frames[frame_num],   
-                                                    track,
-                                                    player_id)
-                tracks['players'][frame_num][player_id]['team'] = team
-                tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
-
-        # estimated the goalkeeper's team based on the teams' centroids
-        team_1_player_bboxes = list(map(lambda t: t[1]["bbox"], filter(lambda t: t[1]["cls_name"] == "player" and t[1]["team"] == 1, player_track.items())))
-        team_2_player_bboxes = list(map(lambda t: t[1]["bbox"], filter(lambda t: t[1]["cls_name"] == "player" and t[1]["team"] == 2, player_track.items())))
-
-        team_1_centroid = calculate_centroid(team_1_player_bboxes)
-        team_2_centroid = calculate_centroid(team_2_player_bboxes)
-        for player_id, track in player_track.items():
-            if track["cls_name"] == "goalkeeper":
-                goalkeeper_bbox = track["bbox"]
-                goalkeeper_center = get_center_of_bbox(goalkeeper_bbox)
-                dist_1 = measure_distance(goalkeeper_center, team_1_centroid)
-                dist_2 = measure_distance(goalkeeper_center, team_2_centroid)
-                if dist_1 < dist_2:
-                    tracks['players'][frame_num][player_id]['team'] = 1
-                    tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[1]
-                else:
-                    tracks['players'][frame_num][player_id]['team'] = 2
-                    tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[2]
-    
+    tracks = team_assigner.get_goalkeeper_team(vod_frames, tracks)
     """
     TRACKING: annotations
     """
-    # # add annotations to match vod 
+    # add annotations to match vod 
     print('adding custom annotations')
     output_frames = tracker.draw_annotations(vod_frames, tracks) 
 
